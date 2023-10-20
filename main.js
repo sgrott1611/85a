8452499e3491bb4642cdd71ac99a907df9e8fb77
sgrott1611 <139928512+sgrott1611@users.noreply.github.com>
2023-10-19 22:03:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+menuListArray = ['Marguerita', 'peperone', '4 queijos'] //Adicione mais nomes de pizza
+
+function getmenu() {
+  var htmldata = ''
+  for (var i = 0; i < menuListArray.length; i++) {
+    htmldata = htmldata + menuListArray[i] + '<br>'
+  }
+  //Dê um nome apropriado ao Id, como displayMenu
+  document.getElementById(' displayMenu        ').innerHTML = htmldata
+}
+
+function add_suggestion() {
+  //Dê um nome de id apropriado, como addItem
+  var item = document.getElementById('addItem ').value
+  //Use a função push() para colocar o item no menuListArray
+  menuListArray.push(item)
+}
